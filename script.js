938859dbe19506a08d286ec9aee6f36f5847f638
Y-S-F/ideas <96319377+youssefhealthylifestyle@users.noreply.github.com>
2022-03-28 20:26:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-   const daysE1 = document.getElementById("days");
+   
    const hoursE1 = document.getElementById("hours");
    const minsE1 = document.getElementById("mins");
     const secondsE1 = document.getElementById("seconds");
 
-const newYears=("29 Mar 2022");
+const newYears=("29 Mar 2022 18:15:00");
 function countdown(){  
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
@@ -13,12 +13,12 @@ function countdown(){
     1000;
 
 
-    const days = Math.floor(totalSeconds/3600/24);
+    
     const hours = Math.floor(totalSeconds/3600)%24;
     const mins = Math.floor(totalSeconds/60)%60;
     const seconds = Math.floor(totalSeconds)%60;
 
-    daysE1.innerHTML=days;
+    
     hoursE1.innerHTML=hours;
     minsE1.innerHTML=formatTime(mins);
     secondsE1.innerHTML=formatTime(seconds);
