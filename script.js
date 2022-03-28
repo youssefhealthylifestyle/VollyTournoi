540c5bbe7c37f0e03be9e7126ef33c98b7ed6949
Y-S-F/ideas <96319377+youssefhealthylifestyle@users.noreply.github.com>
2022-03-28 23:29:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
    const minsE1 = document.getElementById("mins");
     const secondsE1 = document.getElementById("seconds");
 
-const newYears=("29 Mar 2022 18:15:00");
+const newYears=("30 Mar 2022 18:15:00");
 function countdown(){  
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
