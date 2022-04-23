e1ca9c83e86f2458ad2ccd2be06be6dd5a77b300
Y-S-F/ideas <96319377+youssefhealthylifestyle@users.noreply.github.com>
2022-04-23 17:05:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-   const daysE1 = document.getElementById("days");
+   
    const hoursE1 = document.getElementById("hours");
    const minsE1 = document.getElementById("mins");
     const secondsE1 = document.getElementById("seconds");
@@ -13,12 +13,12 @@ function countdown(){
     1000;
 
 
-    const days = Math.floor(totalSeconds/3600/24);
+  
     const hours = Math.floor(totalSeconds/3600)%24;
     const mins = Math.floor(totalSeconds/60)%60;
     const seconds = Math.floor(totalSeconds)%60;
 
-    daysE1.innerHTML=days;
+   
     hoursE1.innerHTML=hours;
     minsE1.innerHTML=formatTime(mins);
     secondsE1.innerHTML=formatTime(seconds);
